e63b8609035b457f19ec01bd6058371c8389b9aa
Rolling <12291690+RollingSmile@users.noreply.github.com>
2025-12-08 03:08:47 +0100

    Update to GuildRoll in standings.lua

--- standings.lua
+++ standings.lua
@@ -4,13 +4,13 @@ local D = AceLibrary("Dewdrop-2.0")
 local C = AceLibrary("Crayon-2.0")
 
 local BC = AceLibrary("Babble-Class-2.2")
-local L = AceLibrary("AceLocale-2.2"):new("retroll")
+local L = AceLibrary("AceLocale-2.2"):new("guildroll")
 local _G = getfenv(0)
 
-RetRoll_standings = RetRoll:NewModule("RetRoll_standings", "AceDB-2.0")
+GuildRoll_standings = GuildRoll:NewModule("GuildRoll_standings", "AceDB-2.0")
 local groupings = {
-  "RetRoll_groupbyclass",
-  "RetRoll_groupbyarmor",
+  "GuildRoll_groupbyclass",
+  "GuildRoll_groupbyarmor",
 }
 local PLATE, MAIL, LEATHER, CLOTH = 4,3,2,1
 local DPS, CASTER, HEALER, TANK = 4,3,2,1
@@ -53,7 +53,7 @@ shooty_export.action:SetHeight(22)
 shooty_export.action:SetPoint("BOTTOM",0,-20)
 shooty_export.action:SetText("Import")
 shooty_export.action:Hide()
-shooty_export.action:SetScript("OnClick",function() RetRoll_standings.import() end)
+shooty_export.action:SetScript("OnClick",function() GuildRoll_standings.import() end)
 shooty_export.title = shooty_export:CreateFontString(nil,"OVERLAY")
 shooty_export.title:SetPoint("TOP",0,-5)
 shooty_export.title:SetFont("Fonts\\ARIALN.TTF", 12)
@@ -88,16 +88,16 @@ shooty_export.scroll = CreateFrame("ScrollFrame", "shooty_exportscroll", shooty_
 shooty_export.scroll:SetPoint('TOPLEFT', shooty_export, 'TOPLEFT', 8, -30)
 shooty_export.scroll:SetPoint('BOTTOMRIGHT', shooty_export, 'BOTTOMRIGHT', -30, 8)
 shooty_export.scroll:SetScrollChild(shooty_export.edit)
-RetRoll:make_escable("shooty_exportframe","add")
+GuildRoll:make_escable("shooty_exportframe","add")
 
-function RetRoll_standings:Export()
+function GuildRoll_standings:Export()
   shooty_export.action:Hide()
   shooty_export.title:SetText(C:Gold(L["Ctrl-C to copy. Esc to close."]))
   local t = {}
   for i = 1, GetNumGuildMembers(1) do
     local name, _, _, _, class, _, note, officernote, _, _ = GetGuildRosterInfo(i)
-    local ep = (RetRoll:get_ep_v3(name,officernote) or 0) 
-    local gp = (RetRoll:get_gp_v3(name,officernote) or RetRoll.VARS.baseAE) 
+    local ep = (GuildRoll:get_ep_v3(name,officernote) or 0) 
+    local gp = (GuildRoll:get_gp_v3(name,officernote) or GuildRoll.VARS.baseAE) 
     if ep > 0 then
       table.insert(t,{name,ep,gp,ep+math.min(50,gp)})
     end
@@ -113,7 +113,7 @@ function RetRoll_standings:Export()
   shooty_export.AddSelectText(txt)
 end
 
-function RetRoll_standings:Import()
+function GuildRoll_standings:Import()
   if not IsGuildLeader() then return end
   shooty_export.action:Show()
   shooty_export.title:SetText(C:Red("Ctrl-V to paste data. Esc to close."))
@@ -121,13 +121,13 @@ function RetRoll_standings:Import()
   shooty_export:Show()
 end
 
-function RetRoll_standings.import()
+function GuildRoll_standings.import()
   if not IsGuildLeader() then return end
   local text = shooty_export.edit:GetText()
   local t = {}
   local found
   for line in string.gfind(text,"[^\r\n]+") do
-    local name,ep,gp,pr = RetRoll:strsplit(";",line)
+    local name,ep,gp,pr = GuildRoll:strsplit(";",line)
     ep,gp = tonumber(ep),tonumber(gp)--,tonumber(pr)
     if (name) and (ep) and (gp) and (pr) then
       t[name]={ep,gp}
@@ -142,12 +142,12 @@ function RetRoll_standings.import()
       local name_epgp = t[name]
       if (name_epgp) then
         count = count + 1
-        --RetRoll:debugPrint(string.format("%s {%s:%s}",name,name_epgp[1],name_epgp[2])) -- Debug
-        RetRoll:update_epgp_v3(name_epgp[1],name_epgp[2],i,name,officernote)
+        --GuildRoll:debugPrint(string.format("%s {%s:%s}",name,name_epgp[1],name_epgp[2])) -- Debug
+        GuildRoll:update_epgp_v3(name_epgp[1],name_epgp[2],i,name,officernote)
         t[name]=nil
       end
     end
-    RetRoll:defaultPrint(string.format(L["Imported %d members."],count))
+    GuildRoll:defaultPrint(string.format(L["Imported %d members."],count))
     local report = string.format(L["Imported %d members.\n"],count)
     report = string.format(L["%s\nFailed to import:"],report)
     for name,epgp in pairs(t) do
@@ -170,14 +170,14 @@ local class_cache = setmetatable({},{__index = function(t,k)
   end
   return k
 end})
-function RetRoll_standings:getArmorClass(class)
+function GuildRoll_standings:getArmorClass(class)
   class = class_cache[class]
   return class_to_armor[class] or 0
 end
 
-function RetRoll_standings:OnEnable()
-  if not T:IsRegistered("RetRoll_standings") then
-    T:Register("RetRoll_standings",
+function GuildRoll_standings:OnEnable()
+  if not T:IsRegistered("GuildRoll_standings") then
+    T:Register("GuildRoll_standings",
       "children", function()
         T:SetTitle(L["Standings"])
         self:OnTooltipUpdate()
@@ -189,64 +189,64 @@ function RetRoll_standings:OnEnable()
         D:AddLine(
           "text", L["Raid Only"],
           "tooltipText", L["Only show members in raid."],
-          "checked", RetRoll_raidonly,
-          "func", function() RetRoll_standings:ToggleRaidOnly() end
+          "checked", GuildRoll_raidonly,
+          "func", function() GuildRoll_standings:ToggleRaidOnly() end
         )      
         D:AddLine(
           "text", L["Group by class"],
           "tooltipText", L["Group members by class."],
-          "checked", RetRoll_groupbyclass,
-          "func", function() RetRoll_standings:ToggleGroupBy("RetRoll_groupbyclass") end
+          "checked", GuildRoll_groupbyclass,
+          "func", function() GuildRoll_standings:ToggleGroupBy("GuildRoll_groupbyclass") end
         )
         D:AddLine(
           "text", L["Group by armor"],
           "tooltipText", L["Group members by armor."],
-          "checked", RetRoll_groupbyarmor,
-          "func", function() RetRoll_standings:ToggleGroupBy("RetRoll_groupbyarmor") end
+          "checked", GuildRoll_groupbyarmor,
+          "func", function() GuildRoll_standings:ToggleGroupBy("GuildRoll_groupbyarmor") end
         )
         D:AddLine(
           "text", L["Refresh"],
           "tooltipText", L["Refresh window"],
-          "func", function() RetRoll_standings:Refresh() end
+          "func", function() GuildRoll_standings:Refresh() end
         )
         D:AddLine(
           "text", L["Export"],
           "tooltipText", L["Export standings to csv."],
-          "func", function() RetRoll_standings:Export() end
+          "func", function() GuildRoll_standings:Export() end
         )
         if IsGuildLeader() then
           D:AddLine(
           "text", L["Import"],
           "tooltipText", L["Import standings from csv."],
-          "func", function() RetRoll_standings:Import() end
+          "func", function() GuildRoll_standings:Import() end
         )
         end
        	end
     )
   end
-  if not T:IsAttached("RetRoll_standings") then
-    T:Open("RetRoll_standings")
+  if not T:IsAttached("GuildRoll_standings") then
+    T:Open("GuildRoll_standings")
   end
 end
 
-function RetRoll_standings:OnDisable()
-  T:Close("RetRoll_standings")
+function GuildRoll_standings:OnDisable()
+  T:Close("GuildRoll_standings")
 end
 
-function RetRoll_standings:Refresh()
-  T:Refresh("RetRoll_standings")
+function GuildRoll_standings:Refresh()
+  T:Refresh("GuildRoll_standings")
 end
 
-function RetRoll_standings:setHideScript()
+function GuildRoll_standings:setHideScript()
   local i = 1
   local tablet = getglobal(string.format("Tablet20DetachedFrame%d",i))
   while (tablet) and i<100 do
-    if tablet.owner ~= nil and tablet.owner == "RetRoll_standings" then
-      RetRoll:make_escable(string.format("Tablet20DetachedFrame%d",i),"add")
+    if tablet.owner ~= nil and tablet.owner == "GuildRoll_standings" then
+      GuildRoll:make_escable(string.format("Tablet20DetachedFrame%d",i),"add")
       tablet:SetScript("OnHide",nil)
       tablet:SetScript("OnHide",function()
-          if not T:IsAttached("RetRoll_standings") then
-            T:Attach("RetRoll_standings")
+          if not T:IsAttached("GuildRoll_standings") then
+            T:Attach("GuildRoll_standings")
             this:SetScript("OnHide",nil)
           end
         end)
@@ -257,30 +257,30 @@ function RetRoll_standings:setHideScript()
   end  
 end
 
-function RetRoll_standings:Top()
-  if T:IsRegistered("RetRoll_standings") and (T.registry.RetRoll_standings.tooltip) then
-    T.registry.RetRoll_standings.tooltip.scroll=0
+function GuildRoll_standings:Top()
+  if T:IsRegistered("GuildRoll_standings") and (T.registry.GuildRoll_standings.tooltip) then
+    T.registry.GuildRoll_standings.tooltip.scroll=0
   end  
 end
 
-function RetRoll_standings:Toggle(forceShow)
+function GuildRoll_standings:Toggle(forceShow)
   self:Top()
-  if T:IsAttached("RetRoll_standings") then -- hidden
-    T:Detach("RetRoll_standings") -- show
-    if (T:IsLocked("RetRoll_standings")) then
-      T:ToggleLocked("RetRoll_standings")
+  if T:IsAttached("GuildRoll_standings") then -- hidden
+    T:Detach("GuildRoll_standings") -- show
+    if (T:IsLocked("GuildRoll_standings")) then
+      T:ToggleLocked("GuildRoll_standings")
     end
     self:setHideScript()
   else
     if (forceShow) then
-      RetRoll_standings:Refresh()
+      GuildRoll_standings:Refresh()
     else
-      T:Attach("RetRoll_standings") -- hide
+      T:Attach("GuildRoll_standings") -- hide
     end
   end  
 end
 
-function RetRoll_standings:ToggleGroupBy(setting)
+function GuildRoll_standings:ToggleGroupBy(setting)
   for _,value in ipairs(groupings) do
     if value ~= setting then
       _G[value] = false
@@ -291,10 +291,10 @@ function RetRoll_standings:ToggleGroupBy(setting)
   self:Refresh()
 end
 
-function RetRoll_standings:ToggleRaidOnly()
-  RetRoll_raidonly = not RetRoll_raidonly
+function GuildRoll_standings:ToggleRaidOnly()
+  GuildRoll_raidonly = not GuildRoll_raidonly
   self:Top()
-  RetRoll:SetRefresh(true)
+  GuildRoll:SetRefresh(true)
 end
 
 local pr_sorter_standings = function(a,b)
@@ -307,54 +307,54 @@ end
 -- Builds a standings table with record:
 -- name, class, armor_class, roles, EP, GP, PR
 -- and sorted by PR
-function RetRoll_standings:BuildStandingsTable()
+function GuildRoll_standings:BuildStandingsTable()
   local t = { }
   local r = { }
-  if (RetRoll_raidonly) and GetNumRaidMembers() > 0 then
+  if (GuildRoll_raidonly) and GetNumRaidMembers() > 0 then
     for i = 1, GetNumRaidMembers(true) do
       local name, rank, subgroup, level, class, fileName, zone, online, isDead = GetRaidRosterInfo(i) 
       r[name] = true
     end
   end
-  RetRoll.alts = {}
+  GuildRoll.alts = {}
   for i = 1, GetNumGuildMembers(1) do
     local name, _, _, _, class, _, note, officernote, _, _ = GetGuildRosterInfo(i)
-    local ep = (RetRoll:get_ep_v3(name,officernote) or 0) 
-    local gp = (RetRoll:get_gp_v3(name,officernote) or RetRoll.VARS.baseAE)
-    local main, main_class, main_rank = RetRoll:parseAlt(name,officernote)
+    local ep = (GuildRoll:get_ep_v3(name,officernote) or 0) 
+    local gp = (GuildRoll:get_gp_v3(name,officernote) or GuildRoll.VARS.baseAE)
+    local main, main_class, main_rank = GuildRoll:parseAlt(name,officernote)
     
     -- Check if the player is a pug
-    local pugName = RetRoll:getPugName(name)
+    local pugName = GuildRoll:getPugName(name)
     local displayName = pugName and string.format("%s (%s)", pugName, name) or name
 
     if (main) then
-      if ((RetRoll._playerName) and (name == RetRoll._playerName)) then
-        if (not RetRoll_main) or (RetRoll_main and RetRoll_main ~= main) then
-          RetRoll_main = main
-          RetRoll:defaultPrint(L["Your main has been set to %s"],RetRoll_main)
+      if ((GuildRoll._playerName) and (name == GuildRoll._playerName)) then
+        if (not GuildRoll_main) or (GuildRoll_main and GuildRoll_main ~= main) then
+          GuildRoll_main = main
+          GuildRoll:defaultPrint(L["Your main has been set to %s"],GuildRoll_main)
         end
       end
       main = C:Colorize(BC:GetHexColor(main_class), main)
-      RetRoll.alts[main] = RetRoll.alts[main] or {}
-      RetRoll.alts[main][name] = class
+      GuildRoll.alts[main] = GuildRoll.alts[main] or {}
+      GuildRoll.alts[main][name] = class
     end
     local armor_class = self:getArmorClass(class)
     if ep > 0 then
-      if (RetRoll_raidonly) and next(r) then
+      if (GuildRoll_raidonly) and next(r) then
         if r[name] then
-          table.insert(t,{displayName,class,armor_class,ep,gp,(ep+ math.min(RetRoll.VARS.AERollCap,gp)),name})
+          table.insert(t,{displayName,class,armor_class,ep,gp,(ep+ math.min(GuildRoll.VARS.AERollCap,gp)),name})
         end
       else
-        table.insert(t,{displayName,class,armor_class,ep,gp,(ep+ math.min(RetRoll.VARS.AERollCap,gp)),name})
+        table.insert(t,{displayName,class,armor_class,ep,gp,(ep+ math.min(GuildRoll.VARS.AERollCap,gp)),name})
       end
     end
   end
-  if (RetRoll_groupbyclass) then
+  if (GuildRoll_groupbyclass) then
     table.sort(t, function(a,b)
       if (a[2] ~= b[2]) then return a[2] > b[2]
       else return pr_sorter_standings(a,b) end
     end)
-  elseif (RetRoll_groupbyarmor) then
+  elseif (GuildRoll_groupbyarmor) then
     table.sort(t, function(a,b)
       if (a[3] ~= b[3]) then return a[3] > b[3]
       else return pr_sorter_standings(a,b) end
@@ -366,7 +366,7 @@ function RetRoll_standings:BuildStandingsTable()
 end
 
 
-function RetRoll_standings:OnTooltipUpdate()
+function GuildRoll_standings:OnTooltipUpdate()
   -- Create category with 2 columns: Name | EP
   local cat = T:AddCategory(
       "columns", 2,
@@ -377,7 +377,7 @@ function RetRoll_standings:OnTooltipUpdate()
   local separator
   for i = 1, table.getn(t) do
     local displayName, class, armor_class, ep, gp, pr, originalName = unpack(t[i])
-    if (RetRoll_groupbyarmor) then
+    if (GuildRoll_groupbyarmor) then
       if not (separator) then
         separator = armor_text[armor_class]
         if (separator) then
@@ -400,13 +400,13 @@ function RetRoll_standings:OnTooltipUpdate()
 
     local text = C:Colorize(BC:GetHexColor(class), displayName)
     local text2
-    if RetRoll_minPE > 0 and ep < RetRoll_minPE then
+    if GuildRoll_minPE > 0 and ep < GuildRoll_minPE then
       text2 = C:Red(string.format("%.4g", ep))
     else
       text2 = string.format("%.4g", ep)
     end
 
-    if ((RetRoll._playerName) and RetRoll._playerName == originalName) or ((RetRoll_main) and RetRoll_main == originalName) then
+    if ((GuildRoll._playerName) and GuildRoll._playerName == originalName) or ((GuildRoll_main) and GuildRoll_main == originalName) then
       text = string.format("(*)%s",text)
     end
     cat:AddLine(
@@ -416,7 +416,7 @@ function RetRoll_standings:OnTooltipUpdate()
   end
 end
 
--- GLOBALS: RetRoll_saychannel,RetRoll_groupbyclass,RetRoll_groupbyarmor,RetRoll_raidonly,RetRoll_decay,RetRoll_minPE,RetRoll_reservechannel,RetRoll_main,RetRoll_progress,RetR[...]
--- GLOBALS: RetRoll,RetRoll_prices,RetRoll_standings,RetRoll_bids,RetRoll_loot,RetRoll_reserves,RetRollAlts,RetRoll_logs
+-- GLOBALS: GuildRoll_saychannel,GuildRoll_groupbyclass,GuildRoll_groupbyarmor,GuildRoll_raidonly,GuildRoll_decay,GuildRoll_minPE,GuildRoll_reservechannel,GuildRoll_main,GuildRoll_progress,RetR[...]
+-- GLOBALS: GuildRoll,GuildRoll_prices,GuildRoll_standings,GuildRoll_bids,GuildRoll_loot,GuildRoll_reserves,GuildRollAlts,GuildRoll_logs
 
 -- FILE CONTENT END --
