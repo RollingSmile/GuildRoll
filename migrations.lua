d654ecf7d3651d959996e8a9791fa2f1e4b06c67
Rolling <12291690+RollingSmile@users.noreply.github.com>
2025-12-08 03:03:05 +0100

    Update to GuildRoll in migration script

--- migrations.lua
+++ migrations.lua
@@ -1,23 +1,23 @@
-local L = AceLibrary("AceLocale-2.2"):new("retroll")
-function RetRoll:v2tov3()
+local L = AceLibrary("AceLocale-2.2"):new("guildroll")
+function GuildRoll:v2tov3()
   local count = 0
   for i = 1, GetNumGuildMembers(1) do
     local name, _, _, _, class, _, note, officernote, _, _ = GetGuildRosterInfo(i)
-    local epv2 = RetRoll:get_ep_v2(name,note)
-    local gpv2 = RetRoll:get_gp_v2(name,officernote)
-    local epv3 = RetRoll:get_ep_v3(name,officernote)
-    local gpv3 = RetRoll:get_gp_v3(name,officernote)
+    local epv2 = GuildRoll:get_ep_v2(name,note)
+    local gpv2 = GuildRoll:get_gp_v2(name,officernote)
+    local epv3 = GuildRoll:get_ep_v3(name,officernote)
+    local gpv3 = GuildRoll:get_gp_v3(name,officernote)
     if (epv3 and gpv3) then
       -- do nothing, we've migrated already
-    elseif (epv2 and gpv2) and (epv2 > 0 and gpv2 >= RetRoll.VARS.baseAE) then
+    elseif (epv2 and gpv2) and (epv2 > 0 and gpv2 >= GuildRoll.VARS.baseAE) then
       count = count + 1
       -- self:defaultPrint(string.format("MainStandingv2:%s,gpv2:%s,i:%s,n:%s,o:%s",epv2,gpv2,i,name,officernote))
-      RetRoll:update_epgp_v3(epv2,gpv2,i,name,officernote)
+      GuildRoll:update_epgp_v3(epv2,gpv2,i,name,officernote)
     end
   end
   self:defaultPrint(string.format(L["Updated %d members to v3 storage."],count))
-  RetRoll_dbver = 3
+  GuildRoll_dbver = 3
 end
 
--- GLOBALS: RetRoll_saychannel,RetRoll_groupbyclass,RetRoll_groupbyarmor,RetRoll_groupbyrole,RetRoll_raidonly,RetRoll_decay,RetRoll_minPE,RetRoll_reservechannel,RetRoll_main,RetRoll_progress,RetRoll_discount,RetRoll_log,RetRoll_dbver,RetRoll_looted
--- GLOBALS: RetRoll,RetRoll_prices,RetRoll_standings,RetRoll_bids,RetRoll_loot,RetRoll_reserves,RetRollAlts,RetRoll_logs
+-- GLOBALS: GuildRoll_saychannel,GuildRoll_groupbyclass,GuildRoll_groupbyarmor,GuildRoll_groupbyrole,GuildRoll_raidonly,GuildRoll_decay,GuildRoll_minPE,GuildRoll_reservechannel,GuildRoll_main,GuildRoll_progress,GuildRoll_discount,GuildRoll_log,GuildRoll_dbver,GuildRoll_looted
+-- GLOBALS: GuildRoll,GuildRoll_prices,GuildRoll_standings,GuildRoll_bids,GuildRoll_loot,GuildRoll_reserves,GuildRollAlts,GuildRoll_logs
